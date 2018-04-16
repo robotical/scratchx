eaa823823ccbfa99d5e046b19820a36a4fe3a7cd
Angus Pearson <a7cp3ar50n@gmail.com>
2018-04-16 18:24:32 +0100

    Adding simple Python 2/3 local server for Scratch

---
+++ serve.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import sys
+
+try:
+    # Py2
+    import SimpleHTTPServer as httpserve
+except ImportError:
+    # Py3
+    import http.server as httpserve
+
+try:
+    # Py2
+    import SocketServer as socketserver
+except ImportError:
+    # Py3
+    import socketserver
+
+import webbrowser
+
+PORT = 6543
+print("Serving at local port %d ..." % PORT)
+
+Handler = httpserve.SimpleHTTPRequestHandler
+httpd = socketserver.TCPServer(("", PORT), Handler)
+print("Serving on http://localhost:%d" % PORT)
+
+webbrowser.open('http://localhost:%d/?url=http://localhost:%d/scratch_extensions/martyExtended.js#scratch' % (PORT, PORT))
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    print("bye!")
+    sys.exit(0)
